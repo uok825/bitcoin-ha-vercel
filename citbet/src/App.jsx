cc0718d15a854c43e4e5da04724774906753b15c
Feyyaz Numan Cavlak <feyyazcavlak@outlook.com>
2024-11-12 11:39:27 +0700

    Add TT

--- citbet/src/App.jsx
+++ citbet/src/App.jsx
@@ -10,8 +10,13 @@ import { Chatbox, Header, Sidebar } from "./components";
 import SVG from "./assets/sanat.svg";
 import PaymentModal from "./components/ui/PaymentModal";
 
+import TextTransition, { presets } from "react-text-transition";
+
+const TEXTS = ["Forest", "Building", "Tree", "Color"];
+
 function App() {
   const [paymentModalIsOpen, setIsPaymentOpen] = useState(true);
+  const [index, setIndex] = useState(0);
   const account = useAccount();
 
   useEffect(() => {
@@ -20,6 +25,14 @@ function App() {
       console.log(user);
     }
   }, [account]);
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      3000 // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
 
   return (
     <div>
@@ -32,6 +45,13 @@ function App() {
         <img src={SVG} alt="" className={styles.sanat} />
         <div className={styles.inner_container}>
           <Sidebar />
+          <div>
+            <h1>
+              <TextTransition springConfig={presets.wobbly}>
+                {TEXTS[index % TEXTS.length]}
+              </TextTransition>
+            </h1>
+          </div>
           <Chatbox />
         </div>
       </div>
