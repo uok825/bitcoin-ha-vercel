afe72b3406b7b8030b041404f3cfc60c88ac6a79
uok825 <omerleroyunda@gmail.com>
2024-11-12 07:24:23 +0300

    FIX: paymentModal

--- citbet/src/components/ui/PaymentModal/index.jsx
+++ citbet/src/components/ui/PaymentModal/index.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ReactModal from "react-modal";
-
 import styles from "./PaymentModal.module.css";
+import { withdraw } from "../../../utils/fetchFunctions";
 
 const customStyles = {
   content: {
@@ -19,13 +19,26 @@ const customStyles = {
 
 ReactModal.setAppElement("#root");
 
-// eslint-disable-next-line react/prop-types
 function PaymentModal({ modalIsOpen, setIsOpen }) {
   const [isWithdraw, setIsWithdraw] = useState(false);
+  const user = localStorage.getItem("user");
+  const parsedUser = JSON.parse(user);
+  const textAreaRef = useRef(null);
+
+  const withdrawBitcoin = async () => {
+    const amount = document.querySelector(".input").value;
+    const address = document.querySelector(".input").value;
+    await withdraw(parsedUser.mSigWallet, amount, address);
+  };
 
   function closeModal() {
     setIsOpen(false);
   }
+
+  const copyToClipboard = () => {
+    textAreaRef.current.select();
+    document.execCommand("copy");
+  };
 
   return (
     <div>
@@ -41,44 +54,75 @@ function PaymentModal({ modalIsOpen, setIsOpen }) {
               <div className={styles.button_container}>
                 <button
                   type="button"
-                  className={styles.button_primary}
+                  className={
+                    isWithdraw ? styles.button_secondary : styles.button_primary
+                  }
                   onClick={() => setIsWithdraw(false)}
                 >
                   Deposit
                 </button>
                 <button
                   type="button"
-                  className={styles.button_secondary}
+                  className={
+                    isWithdraw ? styles.button_primary : styles.button_secondary
+                  }
                   onClick={() => setIsWithdraw(true)}
                 >
                   Withdraw
                 </button>
               </div>
-              <div className={styles.input_container}>
-                <div>
-                  <h5 className={styles.input_desc}>
-                    Amount to withdraw Bitcoin
-                  </h5>
-                  <input
-                    type="text"
-                    placeholder="Amount"
-                    className={styles.input}
-                  />
+              {!isWithdraw && (
+                <div className={styles.deposit_container}>
+                  <h5 className={styles.input_desc}>Your Wallet Address</h5>
+                  <div className={styles.textarea_container}>
+                    <textarea
+                      ref={textAreaRef}
+                      className={styles.textarea}
+                      value={parsedUser?.mSigWallet || ""}
+                      readOnly
+                    />
+                    <button
+                      onClick={copyToClipboard}
+                      className={styles.copy_button}
+                    >
+                      Copy
+                    </button>
+                  </div>
                 </div>
+              )}
+              <div className={styles.input_container}>
                 {isWithdraw && (
                   <div>
+                    <div>
+                      <h5 className={styles.input_desc}>
+                        Amount to withdraw Bitcoin
+                      </h5>
+                      <input
+                        type="text"
+                        placeholder="Amount"
+                        className={styles.input}
+                      />
+                    </div>
                     <h5 className={styles.input_desc}>Your wallet address</h5>
                     <input
                       type="text"
                       placeholder="Address"
                       className={styles.input}
                     />
+                    <button
+                      className={styles.withdraw_button}
+                      onClick={withdrawBitcoin}
+                    >
+                      Withdraw
+                    </button>
                   </div>
                 )}
               </div>
             </div>
             <div>
-              <button className={styles.cancel}>Cancel</button>
+              <button className={styles.cancel} onClick={closeModal}>
+                Cancel
+              </button>
             </div>
           </div>
         </div>
